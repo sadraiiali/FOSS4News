4cc93982ddcf3ec7d0d1284ed678d5505a70c67b
mehran <mehranzolghadr666@gmail.com>
2020-03-30 14:09:01 +0430

    show all posts of user in home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,38 @@
-@extends('layouts.app')
-
-@section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Dashboard</div>
-
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-
-                        You are logged in!
+@extends('layouts.app') @section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <ul class="list-group text-right">
+                <li class="list-group-item" style="background-color:#343A40; color: white;">همه‌ی پست‌های شما</li>
+                @foreach($posts as $index=>$post)
+                <li class="list-group-item">
+                    <div class="row post text-right mr-0 ml-0">
+                        <div class="col-12 post-body">
+                            <h2>
+                                <a href="{{ $post->link }}" class="" target="_blank">
+                                    <span class="text-secondary number">
+                                        {{ num_to_fa($index + 1) }}
+                                    </span>
+                                    . {{ $post->title }}
+                                </a>
+                                <span class="text-secondary link"><a
+                                        href="{{ route('show_post', ['post' => $post]) }}">(نمایش)</a></span>
+                            </h2>
+                            <h3 class="text-secondary">
+                                امتیاز:
+                                {{ num_to_fa($post->getPointsAttribute()) }}
+                                |
+                                {{ num_to_fa(\Morilog\Jalali\Jalalian::forge($post->created_at)->ago()) }}
+                                | {{ num_to_fa($post->comments_count) }}
+                                <a class="text-secondary" href="{{route('show_post',['post'=>$post])}}">
+                                    {{ __("general.Comment") }}</a>
+                            </h3>
+                        </div>
                     </div>
-                </div>
-            </div>
+                </li>
+                @endforeach
+            </ul>
         </div>
     </div>
+</div>
 @endsection
