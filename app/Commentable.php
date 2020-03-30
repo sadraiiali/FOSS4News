ebd79bdc2378c40033963f83ce8d63f43383cd71
mehran <mehranzolghadr666@gmail.com>
2020-03-30 16:13:54 +0430

    created a trait in order to make objects commentable

---
+++ app/Commentable.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App;
+
+use App\Comment;
+use Illuminate\Support\Facades\Auth;
+
+trait Commentable
+{
+    public function comments()
+    {
+        return $this->morphMany(Comment::class, 'commentable');
+    }
+
+    public function getCommentsCountAttribute()
+    {
+        // count number of comments of object
+        return $this->comments->count();
+    }
+
+    public function comment(string $comment)
+    {
+        if ($user = Auth::user()) {
+
+            $this->comments()->create(
+                [
+                    'user_id' => $user->id,
+                    'body' => $comment,
+                ]
+            );
+            return true;
+        }
+        return false;
+    }
+}
