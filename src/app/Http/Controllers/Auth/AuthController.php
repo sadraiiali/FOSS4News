e791663dc9dd0e9e4d23240dec0cec403c3ed91e
mehran <mehranzolghadr666@gmail.com>
2020-04-18 20:15:03 +0430

    create auth controller to autorize users in API

---
+++ src/app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\User;
+use Carbon\Carbon;
+use Hash;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+}
