feb270c5813329b8b17208c3c531673c5b39830b
mehran <mehranzolghadr666@gmail.com>
2020-04-18 20:18:21 +0430

    sign up method added to API Auth Controller

--- src/app/Http/Controllers/Auth/AuthController.php
+++ src/app/Http/Controllers/Auth/AuthController.php
@@ -11,4 +11,40 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
+    /**
+     * Create New User
+     *
+     * @param Illuminate\Http\Request $request
+     * @return Response
+     */
+    public function signup(Request $request)
+    {
+        $request->validate(
+            [
+                'name' => 'required | string',
+                'email' => 'required | email | unique:users',
+                'password' => 'required|string|confirmed',
+                'role' => 'required | string',
+            ]
+        );
+
+        $user = new User(
+            [
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+                'role' => $request->role,
+            ]
+        );
+
+        if (!$user->save()) {
+            return response()->json(
+                ['message' => 'Something Went Wrong'], 500
+            );
+        }
+
+        return response()->json(
+            ['message' => 'Successfully created user.'], 201
+        );
+    }
 }
